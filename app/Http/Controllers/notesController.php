09ff077c66415c19c9400397fcbaffed31bcc727
Jay D <jaydombroski@gmail.com>
2016-02-28 21:54:58 -0500

    Replaced period with semicolon when updating telegram room

--- app/Http/Controllers/notesController.php
+++ app/Http/Controllers/notesController.php
@@ -54,7 +54,7 @@ class notesController extends Controller
 
         Note::create($input);
 
-		$text = $user['first_name'] . ' added a comment on ' . $request['symbol'] . '\nhttp://team.qilinfinance.com/blocks/' . $request['block_id'];
+		$text = $user['first_name'] . ' added a comment on ' . str_replace('.', ':', $request['symbol']) . ' http://team.qilinfinance.com/blocks/' . $request['block_id'];
 
 		Telegram::sendMessage([
 			'chat_id' => env('TELEGRAM_CHAT_ROOM'),
