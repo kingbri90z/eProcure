7c10f9969509a99b70cd9ac47f04354b83749a5a
vagrant <vagrant@homestead>
2016-06-13 16:23:06 +0000

    sanitize email imapbody

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,36 +10,12 @@ use Auth;
 use Ddeboer\Imap\Server;
 use Ddeboer\Imap\SearchExpression;
 use Ddeboer\Imap\Search\Text\Body;
-
 class NotificationController extends Controller
 {
 
     public static function sendUpdateToAll($blade,$data)
     {
-//        $server = new Server('imap.gmail.com');
-        $server = new Server(
-            "imap.gmail.com", // required
-            "993",     // defaults to 993
-            "imap/ssl/novalidate-cert"    // defaults to '/imap/ssl/validate-cert'
 
-        );
-        $connection = $server->authenticate('r2d2@qilinfinance.com', 'whatpassword');
-        $mailboxes = $connection->getMailboxes();
-        $count=0;
-        foreach ($mailboxes as $mailbox) {
-            // $mailbox is instance of \Ddeboer\Imap\Mailbox
-            $messages = $mailbox->getMessages();
-            foreach ($messages as $message) {
-                if(str_contains($message->getSubject(),"Qilin")) {
-                    dd($message->getBodyText());
-                }
-                // $message is instance of \Ddeboer\Imap\Message
-
-
-
-            }
-           // dd(($messages));
-        }
         //Loop through list and send emails.
 //        $users = User::get();
 //
@@ -52,6 +28,61 @@ class NotificationController extends Controller
 //                });
 //           // }
 //        }
+//
+//               "imap.gmail.com", // required
+//            "993",     // defaults to 993
+//            "imap/ssl/novalidate-cert"    // defaults to '/imap/ssl/validate-cert'
+//
+//        );
+//        $connection = $server->authenticate('r2d2@qilinfinance.com', 'whatpassword');
+//
+
+
+        /* connect to gmail */
+        $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
+        $username = 'r2d2@qilinfinance.com';
+        $password = 'whatpassword';
+
+        /* try to connect */
+        $inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+
+        /* grab emails */
+        $emails = imap_search($inbox,'ALL');
+
+        /* if emails are returned, cycle through each... */
+        if($emails) {
+
+            /* begin output var */
+            $output = '';
+
+            /* put the newest emails on top */
+            rsort($emails);
+
+            /* for every email... */
+            foreach($emails as $email_number) {
+
+                /* get information specific to this email */
+                $overview = imap_fetch_overview($inbox,$email_number,0);
+                $message = imap_fetchbody($inbox,$email_number,2);
+
+              $message=preg_replace("/<signature>.+?<\/signature>/i", "", $message);
+//                $message=preg_replace('"','',$message);
+
+
+               $message=strip_tags($message);
+                $end_position=strpos($message,'On Jun');
+                $message=substr($message, 0, $end_position);
+                dd($message);
+
+            }
+
+            echo $output;
+        }
+
+        /* close the connection */
+        imap_close($inbox);
+
+
     }
     public static function sendNewBlock($data){
 
